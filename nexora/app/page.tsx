51d17f4c6edc355b24539ad53fc8f89fcab9f198
Allan sam <allansam0905@gmail.com>
2026-08-11 19:59:29 +0530

    fix - Removed flex

--- nexora/app/page.tsx
+++ nexora/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/Components/layout/Header";
+import Hero from "@/Components/section/Hero";
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div >
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
