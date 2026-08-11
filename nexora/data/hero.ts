dbc65665c9b4ce169caf5efb7011204405531b63
Allan sam <allansam0905@gmail.com>
2026-08-11 22:01:33 +0530

    file - Hero type ts file added

---
+++ nexora/data/hero.ts
@@ -0,0 +1,16 @@
+import { HeroData } from "@/.next/types/home";
+
+
+export const heroData: HeroData = {
+
+    title: "Lessons and insights",
+    subtitle: "Where to grow your business as a photographer: site or social media?.",
+    cta: {
+        label: "Register",
+        href: "/Register"
+    },
+    image: {
+        src: "/images/hero-image.png",
+        alt: "Hero Image"
+    }
+}
