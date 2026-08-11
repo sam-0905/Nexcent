73964cc05cf5ca0ec8cf4c6929df53a6a24f1f5e
Allan sam <allansam0905@gmail.com>
2026-08-11 19:28:04 +0530

    Styles - updated some Css

--- nexora/Components/layout/Header.tsx
+++ nexora/Components/layout/Header.tsx
@@ -7,9 +7,9 @@ import {headerData} from '@/data/header'
 const Header = () => {
   return (
     <>
-    <header className="flex items-center justify-between p-4 m-1">
+    <header className="flex items-center justify-between p-4 px-5 m-1 mx-2 cursor-pointer w-full">
         <div className="flex items-center space-x-4">
-            <Image src={headerData.logo.src} alt={headerData.logo.alt} width={100} height={50} className="object-contain px-2"/>
+            <Image src={headerData.logo.src} alt={headerData.logo.alt} width={100} height={100} className="object-contain vertical-align-middle"/>
         </div>
         <nav className="flex space-x-4">
           {headerData.navigation.map((item) => (
@@ -20,8 +20,8 @@ const Header = () => {
         </nav>
 
     <div className="flex space-x-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
-        <button>Sign Up</button>
+       <button className= "text-green-500 px-4 py-2 rounded">Login</button>
+      <button className="bg-green-500 text-white px-4 py-2 rounded">Sign up</button>
     </div>
 
     </header>
