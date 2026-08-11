ee2cd2102508cf91fe84fe752f019c68b88509a8
Allan sam <allansam0905@gmail.com>
2026-08-11 20:38:01 +0530

    Added - Data of title , subtitle, button from heroData

--- nexora/Components/section/Hero.tsx
+++ nexora/Components/section/Hero.tsx
@@ -6,17 +6,25 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-   <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-     <h1 className="text-4xl font-bold px-0.5">{heroData.title}
-        <span className="text-green-500 px-0.5">from 8 years</span>
-    </h1>
-    <p className="text-lg text-gray-600 p-5">{heroData.subtitle}</p>
-    <button className="bg-green-500 text-white px-4 py-2 rounded">{heroData.cta.label}</button>
-    <div>
-        <Image src={heroData.image.src} alt={heroData.image.alt} width={300} height={300} className="object-contain vertical-align-middle"/>
-    </div>
+    <div className="flex items-center justify-between gap-10">
+       <div className="flex-1">
+            <h1 className="text-5xl font-bold">
+              {heroData.title}
+              <span className="text-green-500">
+                 from 8 years
+                </span>
+             </h1>
 
-   </div>
+            <p className="p-5 text-lg text-gray-600">
+              {heroData.subtitle}
+             </p>
+
+         <button className="rounded bg-green-500 px-4 py-2 text-white">
+             {heroData.cta.label}
+             </button>
+         </div>
+        
+        </div>
     </>
   )
 }
