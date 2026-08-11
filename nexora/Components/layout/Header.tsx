fc0698eb78e9d8ce1188689f0240fe7d8dc2bd25
Allan sam <allansam0905@gmail.com>
2026-08-11 19:18:11 +0530

    Imported headerData from Data and mapped the navigation and added some css

--- nexora/Components/layout/Header.tsx
+++ nexora/Components/layout/Header.tsx
@@ -2,23 +2,25 @@ import Link from 'next/dist/client/link'
 import Image from 'next/image'
 import React from 'react'
 
+import {headerData} from '@/data/header'
+
 const Header = () => {
   return (
     <>
     <header className="flex items-center justify-between p-4 m-1">
-         <Link href="/">
-            <Image src="/nexcentLogo.png" alt="Nexcent Logo" width={100} height={40} />
-        </Link>
-    <nav className="flex space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/features">Feature</Link>
-        <Link href="/products">Product</Link>
-        <Link href="/testimonials">Testimonials</Link>
-        <Link href="/faq">FAQ</Link>
-    </nav>
+        <div className="flex items-center space-x-4">
+            <Image src={headerData.logo.src} alt={headerData.logo.alt} width={100} height={50} className="object-contain px-2"/>
+        </div>
+        <nav className="flex space-x-4">
+          {headerData.navigation.map((item) => (
+            <Link key={item.label} href={item.href}>
+              {item.label}
+            </Link>
+          ))}
+        </nav>
+
     <div className="flex space-x-4">
-        <button>Login</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
         <button>Sign Up</button>
     </div>
 
