7789222692cd1d090fe0dac2011a8e2997c34038
Allan sam <allansam0905@gmail.com>
2026-08-11 18:04:38 +0530

    Imported Header component

---
+++ nexora/Components/layout/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>Header
+    <section>Nexora</section>
+    <section>
+        <h2>Home</h2>
+        <h2>Services</h2>
+        <h2>Feature</h2>
+        <h2>Product</h2>
+        <h2>Testimonials</h2>
+        <h2>FAQ</h2>
+    </section>
+    <section>
+        <button>Login</button>
+        <button>Sign Up</button>
+    </section>
+
+    </>
+  )
+}
+
+export default Header
