3d9c995a524e543ac791f8cf68f702e652b72fe4
Allan sam <allansam0905@gmail.com>
2026-08-11 19:28:45 +0530

    Added a header router

--- nexora/app/page.tsx
+++ nexora/app/page.tsx
@@ -1,11 +1,9 @@
 import Header from "@/Components/layout/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex">
       <Header />
-
     </div>
   );
 }
