57ef0675c0e2dca899a0d6605ae52f67075a8606
Allan sam <allansam0905@gmail.com>
2026-08-11 18:18:14 +0530

    Imported Link , Image from next and re-arranged

--- nexora/Components/layout/Header.tsx
+++ nexora/Components/layout/Header.tsx
@@ -1,21 +1,25 @@
+import Link from 'next/dist/client/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
-    <>Header
-    <section>Nexora</section>
-    <section>
-        <h2>Home</h2>
-        <h2>Services</h2>
-        <h2>Feature</h2>
-        <h2>Product</h2>
-        <h2>Testimonials</h2>
-        <h2>FAQ</h2>
-    </section>
-    <section>
+    <>
+        <Link href="/">
+            <Image src="/nexcentLogo.png" alt="Nexcent Logo" width={100} height={40} />
+        </Link>
+    <nav>
+        <Link href="/">Home</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/features">Feature</Link>
+        <Link href="/products">Product</Link>
+        <Link href="/testimonials">Testimonials</Link>
+        <Link href="/faq">FAQ</Link>
+    </nav>
+    <div>
         <button>Login</button>
         <button>Sign Up</button>
-    </section>
+    </div>
 
     </>
   )
