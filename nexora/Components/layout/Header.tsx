dafbd3b5aff2d459b71c16010f114650aa44e454
Allan sam <allansam0905@gmail.com>
2026-08-11 23:10:08 +0530

    styles - updated a text and bg color with some minor changes

--- nexora/Components/layout/Header.tsx
+++ nexora/Components/layout/Header.tsx
@@ -6,9 +6,9 @@ import {headerData} from '@/data/header'
 const Header = () => {
   return (
     <>
-    <header className="flex items-center justify-between p-4 px-5 m-1 mx-2 cursor-pointer w-full">
-        <div className="flex items-center space-x-4">
-            <Image src={headerData.logo.src} alt={headerData.logo.alt} width={100} height={100} className="object-contain vertical-align-middle"/>
+    <header className="flex items-center justify-between p-4 px-5 m-1 mx-2 cursor-pointer w-full mb-5">
+        <div className="flex items-center space-x-2 m-1 px-3 ">
+            <Image src={headerData.logo.src} alt={headerData.logo.alt} width={150} height={100} className="object-contain vertical-align-middle"/>
         </div>
         <nav className="flex space-x-4">
           {headerData.navigation.map((item) => (
@@ -19,8 +19,8 @@ const Header = () => {
         </nav>
 
     <div className="flex space-x-4">
-       <button className= "text-green-500 px-4 py-2 rounded">Login</button>
-      <button className="bg-green-500 text-white px-4 py-2 rounded">Sign up</button>
+       <button className= "text-[#4caf4f] px-4 py-2 rounded">Login</button>
+      <button className="bg-[#4caf4f] text-white px-4 py-2 rounded">Sign up</button>
     </div>
 
     </header>
