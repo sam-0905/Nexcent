887c02ab29126131f7c0a263e63cbadb485a3fcd
Allan sam <allansam0905@gmail.com>
2026-08-11 22:58:31 +0530

    Added service 1 details to data

---
+++ nexora/data/service.ts
@@ -0,0 +1,24 @@
+import { ServicesData } from "@/types/home";
+
+    
+export const servicesData: ServicesData = {
+    title: "Our Clients",
+    subtitle: "We have been working with some Fortune 500+ clients.",
+    logo:[ 
+        {src: "/icons/Logo-1.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-2.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-3.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-4.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-5.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-6.png",
+        alt: "Company Logo"},
+         {src: "/icons/Logo-7.png",
+        alt: "Company Logo"},
+    ], 
+    
+};
