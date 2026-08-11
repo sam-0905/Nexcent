23951ad6f72dd5cfd6ba6b58f0290b84fc2d3ed3
Allan sam <allansam0905@gmail.com>
2026-08-11 22:00:40 +0530

    styles - Updated the layout also suitable for mobile and applied bg-color

--- nexora/Components/section/Hero.tsx
+++ nexora/Components/section/Hero.tsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-    <div className="flex items-center justify-between gap-10">
-       <div className="flex-1">
-            <h1 className="text-5xl font-bold">
+    <div className="flex flex-col items-center gap-10 md:flex-row md:justify-between bg-[#F5F7FA]">
+       <div className="flex-1 px-8 m-7 justify-center">
+            <h1 className="text-5xl font-bold px-2 ">
               {heroData.title}
               <span className="text-green-500">
                  from 8 years
                 </span>
              </h1>
 
-            <p className="p-5 text-lg text-gray-600">
+            <p className="p-4 text-lg text-gray-400">
               {heroData.subtitle}
              </p>
 
@@ -23,6 +23,16 @@ const Hero = () => {
              {heroData.cta.label}
              </button>
          </div>
+
+           <div className="flex-1">
+         <Image
+            src={heroData.image.src}
+            alt={heroData.image.alt}
+            width={300}
+            height={300}
+            className="object-contain"
+              />
+            </div>
         
         </div>
     </>
