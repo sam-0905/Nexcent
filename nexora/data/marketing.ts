ad2e3c1e58243f4939bd2988819dc71ce6c50db6
Allan sam <allansam0905@gmail.com>
2026-08-12 17:05:04 +0530

    feat(Marketing): add initial TypeScript data and type definitions

---
+++ nexora/data/marketing.ts
@@ -0,0 +1,45 @@
+import { MarketingData } from "@/.next/types/home"
+
+export const marketing :MarketingData = {
+    title:"Caring is the new marketing",
+    subtitle:"The Nexcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​",
+    card:[{
+        logo:{
+        src: "/images/image1.png",
+        alt: "image1",
+    },
+    title: "Creating Streamlined Safeguarding Processes with OneRen",
+    cta:{
+        label: "Readmore",
+        href: "/Readmore",
+    },
+    },
+    {
+        logo:{
+        src: "/images/image2.png",
+        alt: "image2",
+    },
+    title: "What are your safeguarding responsibilities and how can you manage them?",
+    cta:{
+        label: "Readmore",
+        href: "/Readmore",
+    },
+    },
+    {
+        logo:{
+        src: "/images/image3.png",
+        alt: "image3",
+    },
+    title: "Revamping the Membership Model with Triathlon Australia",
+    cta:{
+        label: "Readmore",
+        href: "/Readmore",
+    },
+    }
+],
+    heading:"Pellentesque suscipit fringilla libero eu.",
+     cta:{
+        label: "Get a Demo",
+        href: "/Get a Demo",
+    },
+}
