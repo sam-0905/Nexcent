a83bae40223adc893250ddad602d539116c04792
Allan sam <allansam0905@gmail.com>
2026-08-12 15:03:13 +0530

    feat(data): add initial TypeScript data and type definitions

---
+++ nexora/data/testimonial.ts
@@ -0,0 +1,15 @@
+import { TestimonialData } from "@/.next/types/home"
+
+export const testimonial :TestimonialData = {
+    logo:{
+        src:"/images/pana.png",
+        alt: "pana"
+    },
+    title: "How to design your site footer like we did",
+    para:"Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.",
+
+    cta:{
+    label:"Learn more",
+    href:"/Learn more",
+    },
+}
