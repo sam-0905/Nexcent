a82f75a7cc3b574c1804195740fb6217e8f17dff
Allan sam <allansam0905@gmail.com>
2026-08-12 20:23:30 +0530

    feat(footer): added footer type interface

---
+++ nexora/app/data/footer.ts
@@ -0,0 +1,22 @@
+import {Footer} from "../types/home";
+
+
+export const footer: Footer = {
+    logo:{
+        src:"nexcent-logo-black.png",
+        alt:"logo"
+    },
+    copyRight:"Copyright © 2026 Nexcent ltd",
+    text:"All rights reserved",  
+    company:[{
+         about: "About",
+         blog: "Blog",
+        contactUs: "contact",
+        price: "price",
+    }],
+    date:"Stay up-to date",
+    input:{
+        type:"string",
+        placeholder:"sting",
+    },
+}
