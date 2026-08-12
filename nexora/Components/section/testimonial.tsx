c09eb277a27e56dd2ff9b42fd536070ef8efcfa2
Allan sam <allansam0905@gmail.com>
2026-08-12 15:35:51 +0530

    styles- minor updates in h2 and p

--- nexora/Components/section/testimonial.tsx
+++ nexora/Components/section/testimonial.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const Testimonials = () => {
     return (  <>
     <div className="flex flex-col items-center justify-between mt-10 gap-3 ml-auto md:flex-row">
-        <div className="flex-1">
+        <div className="flex-1 mt-3 mb-3">
             <Image
             src={testimonial.logo.src}
             alt={testimonial.logo.alt}
@@ -13,12 +13,13 @@ const Testimonials = () => {
             height={400}
             />
         </div>
-        <div className="flex-1 space-y-10 m-1 py-2">
+        <div className="flex-1 space-y-10 m-1 py-2 mb-5 mr-4">
             <h2 className="text-3xl font-bold text-[#4D4D4D] mb-7">{testimonial.title}</h2>
-            <p>{testimonial.para}</p>
-            <Link href={testimonial.cta.href}>{testimonial.cta.label}</Link>
+            <p className="text-md text-[#666] mb-8">{testimonial.para}</p>
+            <Link className="bg-[#4caf4f] text-white px-5 py-3 rounded" href={testimonial.cta.href}>{testimonial.cta.label}</Link>
         </div>
     </div>
+
     </>)
   
 }
