63d4d5f2e261142a9f33d9756e453928772ea6d1
Allan sam <allansam0905@gmail.com>
2026-08-12 14:42:44 +0530

    Style -achievement section using CSS grid column layout

--- nexora/Components/section/support.tsx
+++ nexora/Components/section/support.tsx
@@ -4,24 +4,28 @@ import {supportData} from "@/data/support"
 const Support = () => {
   return (
     <>
-    <div className="flex flex-col items-center md:flex-row gap-10 mt-10 mb-8 justify-between">
+    <div className="flex flex-col items-center md:flex-row gap-5 mt-10 mb-8 justify-between">
+
         <div className="flex-1 px-6 m-7 ">
         <h2 className="text-4xl font-bold text-[#4D4D4D] mb-7">{supportData.status.title}
           <span className="block text-[#4caf4f]">business reinvent itself</span>
         </h2>
         <p className="text-md text-[#666] mb-8">{supportData.status.subtitle}</p>
         </div>
-        <div className="flex">
+
+        <div className="grid grid-cols-2 gap-x-16 gap-y-8 mr-8">
         {supportData.status.achievements.map((achievement) => (
-            <div key={achievement.title}>
+            <div key={achievement.title} className="flex items-center gap-4">
             <Image 
                 src={achievement.logo.src} 
                 alt={achievement.logo.alt}  
                 width={50}
                 height={50}
                 />
-                <h3>{achievement.title}</h3>
-                <p>{achievement.subtitle}</p>
+                <div>
+                  <h3 className="font-bold text-[#4D4D4D]" >{achievement.title}</h3>
+                <p className="text-sm text-[#666]">{achievement.subtitle}</p>
+                </div>
             </div>
         ))}
         </div>
