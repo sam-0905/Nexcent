81c79f70d6f112c5ebe7f0ac8e3d01df9c4431f9
Allan sam <allansam0905@gmail.com>
2026-08-12 15:14:15 +0530

    styles - Adjust spacing and layout for mobile responsiveness

--- nexora/Components/section/testimonial.tsx
+++ nexora/Components/section/testimonial.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 const Testimonials = () => {
     return (  <>
-    <div>
-        <div>
+    <div className="flex flex-col items-center justify-between mt-10 gap-3 ml-auto md:flex-row">
+        <div className="flex-1">
             <Image
             src={testimonial.logo.src}
             alt={testimonial.logo.alt}
@@ -13,8 +13,8 @@ const Testimonials = () => {
             height={400}
             />
         </div>
-        <div>
-            <h2>{testimonial.title}</h2>
+        <div className="flex-1 space-y-10 m-1 py-2">
+            <h2 className="text-3xl font-bold text-[#4D4D4D] mb-7">{testimonial.title}</h2>
             <p>{testimonial.para}</p>
             <Link href={testimonial.cta.href}>{testimonial.cta.label}</Link>
         </div>
