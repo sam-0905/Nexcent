f34e35ae3dde5e7d46a227ff617619d5bc55ef09
Allan sam <allansam0905@gmail.com>
2026-08-12 15:35:03 +0530

    feat(data): add additional for Company TypeScript data and type definitions"

--- nexora/data/testimonial.ts
+++ nexora/data/testimonial.ts
@@ -12,4 +12,31 @@ export const testimonial :TestimonialData = {
     label:"Learn more",
     href:"/Learn more",
     },
-}
+    company:{
+        logo:{
+            src:"/images/tesla.png",
+            alt:"tesla",
+        },
+        para:"Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.",
+        title:"Tim Smith",
+        para2:"British Dragon Boat Racing Association",
+        brandLogo:[{src: "/icons/Logo-1.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-2.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-3.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-4.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-5.png",
+        alt: "Company Logo"},
+        {src: "/icons/Logo-6.png",
+        alt:"Company Logo"
+    }],
+        cta:{
+            label:"Meet all customers",
+            href:"/Meet all customers",
+
+        }
+    }
+}   
