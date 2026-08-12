a42f62ac651e900231bf6d8aa5515e5bf44ab1ed
Allan sam <allansam0905@gmail.com>
2026-08-12 11:51:42 +0530

    feat - Added a data for status

---
+++ nexora/data/support.ts
@@ -0,0 +1,44 @@
+import { SupportData } from "@/.next/types/home"
+
+export const supportData : SupportData = {
+    status:{
+    title:"Helping a local business reinvent itself",
+    subtitle:"We reached here with our hard work and dedication",
+    achievements:[
+        {
+            logo: {
+                src: "/icons/members",
+                alt: "members"
+            },
+            title: "2,245,341",
+            subtitle: "Members"
+        },
+         {
+            logo: {
+                src: "/icons/clubs",
+                alt: "clubs"
+            },
+            title: "46,328",
+            subtitle:"clubs"
+        },
+         {
+            logo: {
+                src: "/icons/bookings",
+                alt: "bookings"
+            },
+            title: "828,687",
+            subtitle: "bookings"
+        },
+         {
+            logo: {
+                src: "/icons/Payments",
+                alt: "Payments"
+            },
+            title: "1,926,436",
+            subtitle: "Payments"
+        }
+    ]
+    }
+    
+
+}
