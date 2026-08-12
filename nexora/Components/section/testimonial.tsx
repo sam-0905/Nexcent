5a7c6683af4b39e45d676f5d9b5125d27bdfb921
Allan sam <allansam0905@gmail.com>
2026-08-12 16:23:25 +0530

    styles - Added some styles for company with mobile responsiveness

--- nexora/Components/section/testimonial.tsx
+++ nexora/Components/section/testimonial.tsx
@@ -4,7 +4,8 @@ import Link from "next/link"
 
 const Testimonials = () => {
     return (  <>
-    <div className="flex flex-col items-center justify-between mt-10 gap-3 ml-auto md:flex-row">
+   <div className="flex flex-col items-center gap-3 bg-[#f5f7fa] px-6 py-10 md:flex-row md:justify-between lg:px-20">
+
         <div className="flex-1 mt-3 mb-3">
             <Image
             src={testimonial.logo.src}
@@ -21,20 +22,28 @@ const Testimonials = () => {
     </div>
 
             
-    <div>
-        <div>
+    <div className="flex flex-col items-center gap-3 bg-[#f5f7fa] px-6 py-10 md:flex-row          md:justify-between lg:px-20">
+        <div className="flex-1 mt-3 mb-3 items-center ml-5">
             <Image
             src={testimonial.company.logo.src}
             alt={testimonial.company.logo.alt}
-            width={400}
-            height={400}
+            width={300}
+            height={300}
             />
         </div>
-        <div>
-            <p>{testimonial.company.para}</p>
-            <h2>{testimonial.company.title}</h2>
-            <p>{testimonial.company.para2}</p>
-            <Link href={testimonial.company.cta.href}>{testimonial.company.cta.label} →</Link>
+        <div className="flex-1 space-y-10 m-1 py-2 mb-5 mr-4">
+            <p className="text-md text-[#666] mb-8">{testimonial.company.para}</p>
+            <h2 className="text-3xl font-bold text-[#4caf4f] mb-7">{testimonial.company.title}</h2>
+            <p className="text-gray-500">{testimonial.company.para2}</p>
+             <div>
+                {testimonial.company.brandLogo.map((logo,index)=>(
+            <Image key={index}src={logo.src}alt={logo.alt} width={50} height={50}/>
+            ))}
+             </div>
+            <Link 
+            className="bg-[#4caf4f] text-white px-5 py-3 rounded"
+            href={testimonial.company.cta.href}>
+                {testimonial.company.cta.label} →</Link>
         </div>
     </div>
     
